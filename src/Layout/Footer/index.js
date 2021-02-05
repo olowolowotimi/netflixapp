b28c1610b0b5aaf88ab890a8993f1645eb14233c
Caleb Olojo <belac335@gmail.com>
2021-02-05 14:03:06 +0100

    fix: reduce footer text font-size

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -1,42 +1,39 @@
-import React from 'react';
+import React from "react";
 import footStyle from "./style/Footer.module.css";
 
 const Footer = () => {
-    return(
-        <div className={footStyle.backgroundcolor}>
-            <div className={footStyle.content}>
-            <h4>Questions? Contact us.</h4>
-            <div className={footStyle.section}>
-                <div className={footStyle.sectionOne}>
-                    <p>Faq</p>
-                    <p>Investor Relations</p>
-                    <p>Privacy</p>
-                    <p>Speed Test</p>
-                </div>
-                <div className={footStyle.sectionTwo}>
-                    <p>Help Centers</p>
-                    <p>Jobs</p>
-                    <p>Cookie Preferences</p>
-                    <p>Legal Notices</p>
-                </div>
-                <div className={footStyle.sectionThree}>
-                    <p>Account</p>
-                    <p>Coperate Informations</p>
-                    <p>Ways to Watch</p>
-                    <p>Netflix Originals</p>
-                </div>
-                <div className={footStyle.sectionOne}>
-                    <p>Media Center</p>
-                    <p>Terms of use</p>
-                    <p>Contact us</p>
-                    
-                </div>
-                </div>
-            </div>
-
-
+  return (
+    <div className={footStyle.backgroundcolor}>
+      <div className={footStyle.content}>
+        <h4>Questions? Contact us.</h4>
+        <div className={footStyle.section}>
+          <div className={footStyle.sectionOne}>
+            <p>Faq</p>
+            <p>Investor Relations</p>
+            <p>Privacy</p>
+            <p>Speed Test</p>
+          </div>
+          <div className={footStyle.sectionTwo}>
+            <p>Help Centers</p>
+            <p>Jobs</p>
+            <p>Cookie Preferences</p>
+            <p>Legal Notices</p>
+          </div>
+          <div className={footStyle.sectionThree}>
+            <p>Account</p>
+            <p>Coperate Informations</p>
+            <p>Ways to Watch</p>
+            <p>Netflix Originals</p>
+          </div>
+          <div className={footStyle.sectionOne}>
+            <p>Media Center</p>
+            <p>Terms of use</p>
+            <p>Contact us</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
